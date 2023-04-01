5288b1135a49a7d901ee37320fbe2cb6d90e7ef3
Dev-knowledge <utzsv1989@gmail.com>
2023-04-01 17:18:00 +0000

    second commit

--- src/Hero_section.js
+++ src/Hero_section.js
@@ -1,6 +1,7 @@
 import React from "react";
 import hero_img from "./images/hero-girl.png"
 import hero_design from "./images/hero-waves.png"
+import hero_dots from "./images/hero-dots.png"
 
 
 
@@ -18,6 +19,12 @@ export default function Hero_section() {
                 <img className="hero-img" src={hero_img} alt="girl"/>
              </article>
             <img className="hero-design-img" src={hero_design} />
+            <img className="hero-dots-1 hero-dots" src={hero_dots} />
+            <img className="hero-dots-2 hero-dots" src={hero_dots} />
+            <img className="hero-dots-3 hero-dots" src={hero_dots} />
+            <img className="hero-dots-4 hero-dots" src={hero_dots} />
+            <img className="hero-dots-5 hero-dots" src={hero_dots} />
+            <img className="hero-dots-6 hero-dots" src={hero_dots} />
         </section>
     )
 }
